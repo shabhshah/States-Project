fd1b00504e128613994935a3c27b678e13e0f356
Rishabh <rishabhshah28@gmail.com>
2016-03-01 10:13:47 -0500

    Finished text input

--- input.py
+++ input.py
@@ -8,13 +8,21 @@ import textwrap
 print textwrap.fill("Welcome to the State Game input program. Please enter the states and the respective capitals. Your answers will be added to 'StatesAndCapitals.txt'.")
 print ""
 
+fh = open("StatesAndCapitals.txt", "a")
+a = """
+"""
+
 while True:
 	state = raw_input("Please enter state: ")
-	capital = raw_input("Please enter capital: ")
+	capital = raw_input("Please enter its capital: ")
+
+	print capital + ", " + state
+
+	fh.write(capital + "," + state)
+	fh.write(a)
 
 	print ""
 
-	print capital + ", " + state
 	while True:
 		playAgain = raw_input("Would you like to add another state?: ")
 		if playAgain == "no" or playAgain == "n":
@@ -22,6 +30,7 @@ while True:
 			print "Goodbye"
 			sys.exit()
 		if playAgain == "yes" or playAgain == "y":
+			print ""
 			break
 		if playAgain != "yes" or playAgain != "y" or playAgain != "no" or playAgain != "n":
 			print "You entered something that does not compute. Please try again."
