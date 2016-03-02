730454d99c724a6ac2dd2c5c66d018bbf35c4810
Rishabh <rishabhshah28@gmail.com>
2016-03-02 08:28:52 -0500

    Added scoring functionality.

--- game.py
+++ game.py
@@ -8,13 +8,16 @@ import random
 
 fh = open("StatesAndCapitals.txt", "r")
 statesAndCapitals = []
+score = 0
+wrong = 0
+right = 0
 
 for line in fh:
 	line = line.strip()
 	statesAndCapitals.append(line)
 
 print ""
-print textwrap.fill("Welcome to Rishabh's State Capitals Game! You will be given a state and you need to answer with the correct capital.")
+print textwrap.fill("Welcome to Rishabh's State Capitals Game! You will be given a state and you need to answer with the correct capital. A correct answer is worth 5 points and an incorrect answer is worth -10 points.")
 print ""
 
 while True:
@@ -27,14 +30,24 @@ while True:
 		while True:
 			answer = raw_input("What is the capital of " + state + "?: ")
 			if answer == capital:
+				score += 5
+				right += 1
 				print "You got it correct!"
+				print "Your score is " + str(score) + "."
 				break
 			else:
-				print "You got it wrong! Try again."
+				score -= 10
+				wrong += 1
+				print "You got it wrong!"
+				print "Your score is " + str(score) + "."
+				break
 		while True:
 			print ""
 			playAgain = raw_input("Would you like to play another state?: ")
 			if playAgain == "no" or playAgain == "n":
+				print ""
+				print textwrap.fill("You finished the game with a final score of " + str(score) \
+					                + ". You got " + str(right) + " correct and " + str(wrong) + " incorrect.")
 				print ""
 				print "Goodbye"
 				sys.exit()
@@ -46,5 +59,8 @@ while True:
 		count -= 1
 		if count == 0:
 			print ""
-			print "You finished the game. Goodbye"
+			print textwrap.fill("You finished the game with a final score of " + str(score) \
+				                + ". You got " + str(right) + " correct and " + str(wrong) + " incorrect.")
+			print ""
+			print "Goodbye"
 			sys.exit()
